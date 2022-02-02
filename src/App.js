2f3aecb2856063162d735ecd4c38210f151a2715
nexusxD <elopezr1402@alumno.ipn.mx>
2022-02-01 22:05:36 -0600

    Pruebita

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
 import logo from './logo.svg';
 import './App.css';
-import './views/Mapa.js'
+import Mapa from './views/Mapa.js'
+import { BrowserRouter as Router, Route, Link, Routes } from "react-router-dom";
 
 function App() {
-  return ( <div>
-    <Mapa/>
-  </div> );
+  return ( 
+  <Router>
+    <Routes>
+      <Route path='/Mapa' element={<Mapa V1={2}/>}>
+        
+      </Route>
+    </Routes>
+  </Router> );
 }
 
 export default App;
