c88db4b42ab6c9830c028ac8ed59cc2ac185d7d7
nexusxD <elopezr1402@alumno.ipn.mx>
2022-02-03 22:10:07 -0600

    Mapa con selector de estado

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
-import Mapa from './views/Mapa.js'
+import Mapa from './components/Mapa/Mapa.js'
 import { BrowserRouter as Router, Route, Link, Routes } from "react-router-dom";
 
 function App() {
   return ( 
   <Router>
     <Routes>
-      <Route path='/Mapa' element={<Mapa V1={2}/>}>
+      <Route path='/Mapa' element={<Mapa/>}>
         
       </Route>
     </Routes>
