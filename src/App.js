7a54a0da199f40f9e9b2da62a29cbfd1fe593311
nexusxD <elopezr1402@alumno.ipn.mx>
2022-02-20 20:14:41 -0600

    Mapas con fallas en css

--- src/App.js
+++ src/App.js
@@ -1,15 +1,17 @@
 import logo from './logo.svg';
 import './App.css';
-import Mapa from './components/Mapa/Mapa.js'
+import Mapa from './components/Mapa/Mapa.jsx'
+import MapaCul from './components/MapaCul/Mapa.jsx'
+import MapaReg from './components/MapaReg/Mapa.jsx'
 import { BrowserRouter as Router, Route, Link, Routes } from "react-router-dom";
 
 function App() {
   return ( 
   <Router>
     <Routes>
-      <Route path='/Mapa' element={<Mapa/>}>
-        
-      </Route>
+      <Route path='/Mapa' element={<Mapa/>}> </Route>
+      <Route path='/MapaCulturas' element={<MapaCul/>}> </Route>
+      <Route path='/MapaRegiones' element={<MapaReg/>}> </Route>
     </Routes>
   </Router> );
 }
---
+++ src/components/MapaReg/components/MapaObjeto.jsx
@@ -0,0 +1,11 @@
+import react from "react";
+function MapaObjeto(props){
+    return(<>
+        <div>
+            <h1>{props.keyProp}</h1>
+            
+        </div>
+
+    </>)
+    ;}
+export default MapaObjeto;
