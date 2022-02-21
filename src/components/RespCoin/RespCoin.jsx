ab017a028275222c2e98e94cb27e2ee128c6e916
MagnesiaReal <mgarridor1400@alumno.ipn.mx>
2022-02-21 19:45:33 +0000

    dir for respuestas coincidente

---
+++ src/components/RespCoin/RespCoin.jsx
@@ -0,0 +1,21 @@
+import react from 'react'
+
+import './RespCoin.css'
+
+class RespCoin extends react.Component {
+  constructor(props) {
+    super(props);
+
+  }
+
+  render() {
+    
+    return (
+      <div className="uunuur">
+        soy un texto alv
+      </div>
+    );
+  }
+}
+
+export default RespCoin;
