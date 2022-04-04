2fd85e9460c2766e6483e92ab5cf834d4ffa4cac
MagnesiaReal <mgarridor1400@alumno.ipn.mx>
2022-04-04 16:21:41 -0500

    before merge

---
+++ src/views/ChangePass/ChangePass.jsx
@@ -0,0 +1,216 @@
+import React from 'react';
+import {useParams, useNavigate} from 'react-router-dom';
+import { passwordStrength } from 'check-password-strength';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+
+import AXIOS from '../../components/http-axios';
+
+
+class WrappedChangePass extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      newPass: "",
+      newPassR: "",
+      targetEmail: "",
+      passwordStrength: "",
+      errorPass: "",
+      changeSuccessfully: false,
+      eye: "fas fa-eye-slash",
+      btnClass: "btn btn-dark"
+    };
+
+    this.passOptions = [
+      {
+        id: 0,
+        value: "Muy Debil   :(",
+        minDiversity: 0,
+        minLength: 0
+      },
+      {
+        id: 1,
+        value: "Debil    :|",
+        minDiversity: 2,
+        minLength: 6
+      },
+      {
+        id: 2,
+        value: "Mediana     :O",
+        minDiversity: 4,
+        minLength: 8
+      },
+      {
+        id: 3,
+        value: "Fuerte     :D",
+        minDiversity: 4,
+        minLength: 10
+      }
+    ]; 
+
+    this.onSubmitNewPass = this.onSubmitNewPass.bind(this);
+    this.onPass = this.onPass.bind(this);
+    this.onPassR = this.onPassR.bind(this);
+    this.onShowPass = this.onShowPass.bind(this);
+
+    this.refPass = React.createRef(null);
+    this.refPassR = React.createRef(null);
+  }
+
+  componentDidMount() {
+    console.log(this.props.params.code);
+    
+    AXIOS.post('/verifychangepass', {code: this.props.params.code})
+    .then((res)=> {
+      
+      console.log("CHANGEPASS>> ",res.data);
+      this.setState({targetEmail: res.data.email});    
+
+    }).catch(err => {
+
+      console.log("CHANGEPASS>> ",err.response.data.message);
+      this.props.navigation('/home');
+
+    });
+
+  }
+
+  onSubmitNewPass (e) {
+    e.preventDefault();
+    console.log(this.props.params.code);
+
+    if(this.state.newPass === this.state.newPassR) {
+      if(passwordStrength(this.state.newPass, this.passOptions).id > 1) {
+
+        const credentials = {
+          code: this.props.params.code,
+          pass: this.state.newPass
+        }
+
+        AXIOS.post('/changepass', credentials)
+        .then(res => {
+          
+          console.log("CHANGEPASS>> ", res.data.message);
+          this.setState({changeSuccessfully: true});
+
+          setTimeout(()=>{
+            this.props.navigation('/login');
+          },5000);
+
+        }).catch(err => {
+          
+          console.log("CHANGEPASS>> error: ", err);
+          
+        });
+
+      } else {<i class="fa-solid fa-eye"></i>
+        this.setState({errorPass: <small>la contraseña tiene que ser medianamente fuerte</small>});
+      }
+    } else {
+      this.setState({errorPass: <small>las contraseñas no coinciden</small>});
+    }
+
+    
+
+  }
+
+  onPass(e) {
+    this.setState({newPass: e.target.value, passStrength: <small>{passwordStrength(e.target.value, this.passOptions).value}</small>});
+  }
+
+  onPassR(e) {
+    this.setState({newPassR: e.target.value});
+  }
+
+  onShowPass(e) {
+    e.preventDefault();
+    if(this.refPass.current.type === "password") {
+      this.refPass.current.type = "text";
+      this.refPassR.current.type = "text";
+      this.setState({eye: "fa-solid fa-eye", btnClass: "btn btn-light"});
+
+    } else {
+      this.refPass.current.type = "password";
+      this.refPassR.current.type = "password";
+      this.setState({eye: "fas fa-eye-slash", btnClass: "btn btn-dark"});
+    }
+
+  }
+
+  render() {
+    
+    if(this.state.changeSuccessfully) return(
+      <div className="register-stea">
+        <div id="propiedades-stea" className="container">
+          <div className="col" align="center">
+            <div className="d-inline-block align-middle">
+              <h2>Usuario Verificado !!!</h2>
+            </div>
+          </div>
+          <div className="este"></div>
+          <div className="dropdown-divider"/>
+          <div className="container">
+            <div className="row">
+              <div className="col-12 stea-register-desc m-2">
+                <p align="justify">
+                Contraseña cambiada con exito, serás redireccionado al inicio de sesión en algunos segundos. :)
+                </p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    );
+
+    return(
+      <div className="login-stea">
+          <div id="propiedades-stea" className="container">
+            <div className="col" align="center">
+              <div className="d-inline-block align-middle">
+                <h2>Cambiar contraseña para {this.state.targetEmail}</h2>
+              </div>
+            </div>
+
+            <div className="dropdown-divider"></div>
+            <div className="container">
+              <form onSubmit={this.onSubmitNewPass}>
+                <div className="col-12 p-1" align="center">
+                  <br/>
+                  <label htmlFor="1">
+                    Nueva contraseña:
+                  </label><br/>
+                  <input id="1" className="stea-login-input-text" type="password" ref={this.refPass} value={this.state.newPass} onChange={this.onPass}/><br/>
+                  {this.state.passStrength}
+                </div>
+
+                <div className="col-12 p-1" align="center">
+                  <br/>
+                  <label htmlFor="2">
+                    Repetir nueva contraseña:
+                  </label><br/>
+                  <input id="2" className="stea-login-input-text" type="password" ref={this.refPassR} value={this.state.newPassR} onChange={this.onPassR}/><br/>
+                  {this.state.errorPass}
+                </div>
+                  
+                <div className="col-12 col-sm-5 p-1">
+                  <button className={this.state.btnClass} onClick={this.onShowPass}><FontAwesomeIcon icon={this.state.eye}/></button> Mostrar contraseña
+                </div>
+
+                <div className="col-12 p-1" align="center">
+                  <input type="submit" value="Reestablecer contraseña" className="btn btn-dark"/>
+                </div>
+              </form>
+            </div>
+          </div>
+        </div>
+    );
+  }
+
+}
+
+
+export default function ChangePass(props) {
+  const params = useParams();
+  const navigation = useNavigate();
+  return <WrappedChangePass {...props} params={params} navigation={navigation}/>
+}
--- src/views/Register/Registrer.jsx
+++ src/views/Register/Registrer.jsx
@@ -1,42 +1,197 @@
-import React from 'react'
+import React, {useEffect} from 'react'
 import AXIOS from '../../components/http-axios'
-import { Link } from 'react-router-dom'
+import { useNavigate } from 'react-router-dom'
+import { passwordStrength } from 'check-password-strength'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 
-export default class Register extends React.Component {
+import './Registrer.css'
+
+class WrappedRegister extends React.Component {
   constructor(props){
     super(props);
+
     this.state = {
       name: "",
       lastName: "",
       email: "",
+      born:"",
       pass: "",
-      passr: ""
+      passr: "",
+      verifyview: false,
+      code: "",
+      successRegister: false,
+      eye: "fas fa-eye-slash",
+      btnClass: "btn btn-dark"
     };
-    this.onLogin = this.onLogin.bind(this);
+
+    this.passOptions = [
+      {
+        id: 0,
+        value: "Muy Debil   :(",
+        minDiversity: 0,
+        minLength: 0
+      },
+      {
+        id: 1,
+        value: "Debil    :|",
+        minDiversity: 2,
+        minLength: 6
+      },
+      {
+        id: 2,
+        value: "Mediana     :O",
+        minDiversity: 4,
+        minLength: 8
+      },
+      {
+        id: 3,
+        value: "Fuerte     :D",
+        minDiversity: 4,
+        minLength: 10
+      }
+    ];
+
+    this.onVerify = this.onVerify.bind(this);
+    this.onCode = this.onCode.bind(this);
+    this.onResend = this.onResend.bind(this);
+
+    this.onRegister = this.onRegister.bind(this);
     this.onName = this.onName.bind(this);
     this.onLastName = this.onLastName.bind(this);
     this.onEmail = this.onEmail.bind(this);
+    this.onBorn = this.onBorn.bind(this);
     this.onPass = this.onPass.bind(this);
     this.onPassR = this.onPassR.bind(this);
+    this.onShowPass = this.onShowPass.bind(this);
+
+    this.refEmail = React.createRef(null);
+    this.refPass = React.createRef(null);
+    this.refPassr = React.createRef(null);
+
+    if(this.props.cookie.get('UUID')) this.props.navigation(`/user/${this.props.cookie.get('userId')}`);
   }
 
-  onLogin(e){
+  onVerify(e) {
     e.preventDefault();
-    if (this.state.pass === this.state.passr) {
-      const newUserCredentials={
+    console.log("REGISTER>> Verifing user");
+
+    const newUserCredentials = {
       name: this.state.name,
       lastName: this.state.lastName,
       email: this.state.email,
-      pass: this.state.pass
-      }
-      AXIOS.post('/register', newUserCredentials)
+      born: this.state.born,
+      pass: this.state.pass,
+      code: this.state.code
+    }
+
+
+    AXIOS.post('/verify', newUserCredentials)
+      .then((res)=> {
+        if (true) {
+          
+          this.props.cookie.set('userId', res.data.idUsuario, { path: '/' });
+          this.props.cookie.set('UUID', res.data.uuid, { path: '/' });
+          this.props.cookie.set('name', this.state.name, { path: '/'});
+          this.props.cookie.set('lastName', this.state.lastName, { path: '/'});
+          this.props.cookie.set('email', this.state.email, { path: '/'});
+          this.props.cookie.set('born', this.state.born, { path: '/'});
+          this.props.cookie.set('admin', 0, { path: '/'});
+          this.props.cookie.set('photo', null, { path: '/'});
+          this.props.cookie.set('configuration', null, { path: '/'});
+          this.props.cookie.set('avatarId', null, { path: '/'});
+
+          this.setState({successRegister: true});
+
+          setTimeout(()=> {
+            this.props.navigation(`/user/${res.data.idUsuario}`);
+          }, 8000);
+
+        }
+      }).catch(err => {
+
+        console.log("REGISTER>> Verify error");
+        switch (err.response.status) {
+          case 500:
+            this.setState({errorCode: <small>* Error en el Serivdor intenta mas tarde</small>});
+            break;
+          case 401:
+            this.setState({errorCode: <small>* Codigo incorrecto !!!</small>});
+            break;
+          default:
+          this.setState({errorCode: <small>* Error desconocido hmm...</small>});
+        }  
+        if (err) throw err;
+
+      })
+
+  }
+
+  onCode(e) {
+    this.setState({code: e.target.value});
+  }
+
+  onResend(e) {
+    e.preventDefault();
+    AXIOS.post('/register', {email: this.state.email})
+      .then(res=> {
+
+        console.log("REGISTER>> ", res);
+        this.setState({verifyview: true});
+
+      }).catch((err)=>{
+
+        if (err.response.status === 409){
+
+          console.log("REGISTER>> Error code: ", err.response.data);
+          this.setState({emailExist: <small>* Ya existe un usuario con este correo</small>})
+          this.refEmail.current.className = `${this.refEmail.current.className} stea-input-error`;
+
+        } else {
+
+          alert("Algo salio mal Intenta mas tarde");
+
+        }
+      }) 
+
+  }
+
+  onRegister(e){
+    e.preventDefault();
+    console.log("REGISTER>> Registering user");
+    if(passwordStrength(this.state.pass, this.passOptions).id < 2) {
+      
+      this.setState({passInfo: <small>* La contraseña debe ser medianamente fuerte</small>});
+      return;
+
+    }
+    if (this.state.pass === this.state.passr) {
+
+      AXIOS.post('/register', {email: this.state.email})
         .then(res=> {
-          console.log(res);
-        }).catch(err=>{
-          throw err;
-        })  
+
+          console.log("REGISTER>> ", res);
+          this.setState({verifyview: true});
+
+        }).catch((err)=>{
+
+          if (err.response.status === 409){
+
+            console.log("REGISTER>> Error code: ", err.response.data);
+            this.setState({emailExist: <small>* Ya existe un usuario con este correo</small>})
+            this.refEmail.current.className = `${this.refEmail.current.className} stea-input-error`;
+
+          } else {
+
+            alert("Algo salio mal Intenta mas tarde");
+
+          }
+        }) 
+
     } else {
-      this.setState({passInfo: <h6>Las contraseñas no coinciden</h6>})
+
+      this.refPassr.current.className = `${this.refPassr.current.className} stea-input-error`;
+      this.setState({passInfo: <small>* Las contraseñas no coinciden</small>})
+
     }
   }
 
@@ -51,20 +206,105 @@ export default class Register extends React.Component {
   onEmail(e){
     this.setState({email: e.target.value});
   }
-  
+
+  onBorn(e){
+    this.setState({born: e.target.value});
+  }
+
   onPass(e){
-    this.setState({pass: e.target.value});
+    this.setState({pass: e.target.value, passStrength: <small>{passwordStrength(e.target.value, this.passOptions).value}</small>});
     console.log(e.target.value);
   }
 
   onPassR(e){
     this.setState({passr: e.target.value});
-    console.log(e.target.value);
+  }
+
+  onShowPass(e) {
+    e.preventDefault();
+    if(this.refPass.current.type === "password") {
+      this.refPass.current.type = "text";
+      this.refPassr.current.type = "text";
+      this.setState({eye: "fa-solid fa-eye", btnClass: "btn btn-light"});
+
+    } else {
+      this.refPass.current.type = "password";
+      this.refPassr.current.type = "password";
+      this.setState({eye: "fas fa-eye-slash", btnClass: "btn btn-dark"});
+    }
+
   }
 
   render(){
+
+    if (this.state.successRegister) {
+      return (
+        <div className="register-stea">
+          <div id="propiedades-stea" className="container">
+            <div className="col" align="center">
+              <div className="d-inline-block align-middle">
+                <h2>Usuario Verificado !!!</h2>
+              </div>
+            </div>
+            <div className="este"></div>
+            <div className="dropdown-divider"/>
+            <div className="container">
+              <div className="row">
+                <div className="col-12 stea-register-desc m-2">
+                  <p align="justify">
+                  Felicidades, tu usuario ha sido verificado, seras redireccionado al inicio de sesión en unos segundos
+                  por favor espera. :)
+                  </p>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      );
+    }
+
+    if (this.state.verifyview) return (
+      <div className="register-stea">
+        <div id="propiedades-stea" className="container">
+          <div className="col" align="center">
+            <div className="d-inline-block align-middle">
+              <h2>Verificar Usuario</h2>
+            </div>
+          </div>
+          <div className="este"></div>
+
+          <div className="dropdown-divider"/>
+
+          <div className="container">
+            <form onSubmit={this.onVerify}>
+              <div className="row">
+                <div className="col-12 stea-register-desc">
+                  <p align="justify">
+                  Hemos enviado un código de verificación en tu correo, por favor escribe 
+                  el codigo que recibiste, el enlace expirara en 30 minutos.
+                  </p>
+                </div>
+                <div className="col-12 p-1" align="center">
+                  <input id="code" placeholder="Codigo" type="text" value={this.state.code} onChange={this.onCode} className="stea-register-input-text" required/>
+                  {this.state.errorCode}
+                </div>
+
+                <div className="col-12 p-1" align="center">
+                  <input type="submit" value="Verificar" className="btn btn-dark"/>
+                </div>
+                <div className="col-12 p-1" align="center">
+                  <a href="##" id="stea-register-resend" onClick={this.onResend}>Reenviar Código</a>
+                </div>
+              </div>
+            </form>
+          </div>
+
+        </div>
+      </div>
+    );
+
     return(
-      <div className="login-stea">
+      <div className="register-stea">
         <div id="propiedades-stea" className="container">
           <div className="col" align="center">
             <div className="d-inline-block align-middle">
@@ -72,59 +312,85 @@ export default class Register extends React.Component {
             </div>
           </div>
           <div className="este"></div>
-          
+
           <div className="dropdown-divider"/>
-          
+
           <div className="container">
-            <form onSubmit={this.onLogin}>
+            <form onSubmit={this.onRegister}>
               <div className="row">
 
-                <div className="col-12 p-1" align="center">
+                <div className="col-12 col-md-6 p-1">
                   <label htmlFor="name">
-                    Nombre:
+                  Nombre:
                   </label><br/>
-                  <input id="name" type="text" value={this.state.name} onChange={this.onName}/>
+                  <input id="name" placeholder="John" type="text" value={this.state.name} onChange={this.onName} className="form-control" required/>
                 </div>
 
-                <div className="col-12 p-1" align="center">
+                <div className="col-12 col-md-6 p-1">
                   <label htmlFor="lastName">
-                    Apellido:
+                  Apellido:
                   </label><br/>
-                  <input id="lastName" type="text" value={this.state.lastName} onChange={this.onLastName}/>
+                  <input id="lastName" placeholder="Cena" type="text" value={this.state.lastName} onChange={this.onLastName} className="form-control" required/>
                 </div>
 
-                <div className="col-12 p-1" align="center">
+                <div className="col-12 col-sm-7 p-1">
                   <label htmlFor="email">
-                    Correo:
+                  Correo:
                   </label><br/>
-                  <input id="email" type="text" value={this.state.email} onChange={this.onEmail}/>
+                  <input id="email" type="text" ref={this.refEmail} value={this.state.email} onChange={this.onEmail} className="form-control" required/>
+                  {this.state.emailExist}
                 </div>
 
-                <div className="col-12 p-1" align="center">
+                <div className="col-12 col-sm-5 p-1">
+                  <label htmlFor="born">
+                  Fecha de nacimiento:
+                  </label>
+                  <input type="date" name="born" id="born" value={this.state.born} onChange={this.onBorn} className="form-control" required/>
+                </div>
+
+
+                <div className="col-12 p-1">
                   <label htmlFor="pass">
-                    Contraseña:
+                  Contraseña:
                   </label><br/>
-                  <input id="pass" type="password" placeholder="" value={this.state.pass} onChange={this.onPass}/>
+                  <input id="pass" ref={this.refPass} type="password" placeholder="" value={this.state.pass} onChange={this.onPass} className="form-control" required/>
+                  {this.state.passStrength}
+                </div>
+
+                <div className="col-12 p-1">
+                  <label htmlFor="passr">
+                  Repetir contraseña:
+                  </label><br/>
+                  <input id="passr" type="password" placeholder="" ref={this.refPassr} value={this.state.passr} onChange={this.onPassR} className="form-control" required/>
                   {this.state.passInfo}
                 </div>
 
-                <div className="col-12 p-1" align="center">
-                  <label htmlFor="passr">
-                    Repetir contraseña:
-                  </label><br/>
-                  <input id="passr" type="password" placeholder="" value={this.state.passr} onChange={this.onPassR}/>
+                <div className="col-12 col-sm-5 p-1">
+                  <button className={this.state.btnClass} onClick={this.onShowPass}><FontAwesomeIcon icon={this.state.eye}/></button> Mostrar contraseña
                 </div>
-                
+
                 <div className="col-12 p-1" align="center">
-                  <input type="submit" value="Entrar" className="btn btn-warning"/>
+                  <input type="submit" value="Entrar" className="btn btn-dark"/>
                 </div>
               </div>
             </form>
           </div>
 
         </div>
-      </div>    );
+      </div>    
+    );
+
   }
 
+}
 
+export default function Register(props){
+
+  const navigation = useNavigate();
+  
+  useEffect(()=>{
+    if(props.cookie.get('UUID')) navigation(`/user/${props.cookie.get('userId')}`);
+  },[]);
+
+  return <WrappedRegister {...props} navigation={navigation}/>
 }
