b3a6c37eefb4aa37780f961864172a68e3450d13
nexusxD <elopezr1402@alumno.ipn.mx>
2022-05-29 23:36:43 -0500

    Puntaje de las actividades según su avatar

--- src/views/Activity/Mapas/Mapas.jsx
+++ src/views/Activity/Mapas/Mapas.jsx
@@ -76,12 +76,17 @@ export default function Mapas(props){
         var calificacion=0;//calificación final
         var califPregunta=0;//calificación de la pregunta actual
         var auxPregunta=0;//acumulador de la calificación final
+        var experienciaMul=1;//Multiplicador de experiencia
+        var experiencia=120;//Experiencia que gana el usuario
+        var seguidasAux=1;//Auxiliar para contar seguidas
+        var puntosIng=1;//Auxiliar para conocer la cantidad de veces que junto 3 preguntas
         if(idAvatar===1){//corredor
           listaRespuestas.forEach((respuesta)=>{
+            califPregunta=0;//Limpiamos el valor de cada pregunta para evitar errores
             if(respuesta.respuestaRes===respuesta.Respuesta){
               correctas++
-              califPregunta=10*respuesta.tiempo;
-              auxPregunta=califPregunta+auxPregunta;
+              califPregunta=10*respuesta.tiempo;//calificación de la pregunta por el tiempo
+              auxPregunta=califPregunta+auxPregunta;//sumatoria de la calificación
               if(respuesta.tiempoRes===4){
                 seguidas++//se incrementa la variable de respuestas seguidas siendo primeros
               }else{
@@ -90,23 +95,74 @@ export default function Mapas(props){
               if(seguidas>=5){//si el corredor tiene más de 5 tiene multiplicador
                 multiplicador=1.05;
               }
+            }else{
+              //No hace nada si no responde bien¿?
             }
           }
           )
           calificacion=auxPregunta*multiplicador;
+          experiencia;
         }else if(idAvatar===2){//profesora
           listaRespuestas.forEach((respuesta)=>{
-
+            califPregunta=0;//Limpiamos el valor de cada pregunta para evitar errores
+            if(respuesta.respuestaRes===respuesta.Respuesta){
+              correctas++
+              califPregunta=10*respuesta.tiempo;//calificación de la pregunta por el tiempo
+              auxPregunta=califPregunta+auxPregunta;//sumatoria de la calificación
+              if(posicion===1||posicion===2||posicion===3){//uso posición porque no se como se puede obtener 
+                experienciaMul=1.1;//Modifica el multiplicador de experiencia
+              }
+            }
           }
           )
+          calificacion=auxPregunta;
+          experiencia=experiencia*experienciaMul;
         }else if(idAvatar===3){//cirujana
           listaRespuestas.forEach((respuesta)=>{
+            califPregunta=0;//Limpiamos el valor de cada pregunta para evitar errores
+            if(respuesta.respuestaRes===respuesta.Respuesta){
+              correctas++
+              califPregunta=10*respuesta.tiempo;//calificación de la pregunta por el tiempo
+              auxPregunta=califPregunta+auxPregunta;//sumatoria de la calificación
+              seguidas++//Cuenta la cantidad de preguntas seguidas que ha contestado
+              
+            }else{
+              seguidasAux=seguidas;//Se guarda el valor de las rachas en caso de que no tengamos respuesta correcta
 
+            }
           }
           )
+          if(seguidasAux>=seguidas){//Compara que valor es el mayor entre las rachas obtenidas
+            multiplicador=1.05*seguidasAux//Si tenemos que la racha historia es mayor a la racha que tenemos se multiplica por el historico
+          }else{
+            multiplicador=1.05*seguidas//Si tenemos que la racha actual es mayor a la historica se multiplica por ese factor
+          }
+          calificacion=auxPregunta*multiplicador;//puntuación con el multiplicador
+          experiencia;//experiencia ganada
         }else if(idAvatar===4){//ingeniero
           listaRespuestas.forEach((respuesta)=>{
-
+          califPregunta=0;//Limpiamos el valor de cada pregunta para evitar errores
+          if(respuesta.respuestaRes===respuesta.Respuesta){
+            correctas++
+            califPregunta=10*respuesta.tiempo;//calificación de la pregunta por el tiempo
+            auxPregunta=califPregunta+auxPregunta;//sumatoria de la calificación
+            seguidas++//Cuenta la cantidad de preguntas seguidas que ha contestado
+          }else{
+              seguidasAux=seguidas;//Se guarda el valor de las rachas en caso de que no tengamos respuesta correcta
+          }
+          if(seguidasAux>=seguidas){//Compara que valor es el mayor entre las rachas obtenidas
+            puntosIng=seguidasAux%3//contamos la cantidad de veces que acumulo 3 respuestas correctas
+            if(puntosIng===0)//si no obtuvo ninguno se pondra como valor 1 para multiplicar
+            puntosIng=1;
+          }else{
+            puntosIng=seguidas%3//contamos la cantidad de veces que acumulo 3 respuestas correctas
+            if(puntosIng===0)//si no obtuvo ninguno se pondra como valor 1 para multiplicar
+            puntosIng=1;
+          }
+          multiplicador=multiplicador+(puntosIng/10);
+          calificacion=(auxPregunta*multiplicador);//puntuación con el multiplicador
+          experiencia;//experiencia ganada 
+          
           }
           )
         }
